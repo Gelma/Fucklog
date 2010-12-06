25a2c8816de291720e6255acbc510ee604d1d641
Nicolo' 'necro' Cristini <necro@circolab.net>
2010-12-06 23:14:32 +0100

    Aggiunto controllo su _fetch_pbl()
    Se non e' settato l'indirizzo ip ritorna con un errore 1

--- pblob/pblob.py
+++ pblob/pblob.py
@@ -10,11 +10,10 @@ class sphPBL:
 	pbl_url = None
 	cidr = None
 
-	_header = { "User-Agent":"Mozilla/5.0 (X11; U; Linux i686; it; rv:1.9.2.12) Gecko/20101027 Ubuntu/10.04 (lucid) Firefox/3.6.12",
-				"Accept":"text/html"}
+	_header = {"User-Agent":"Mozilla/5.0 (X11; U; Linux i686; it; rv:1.9.2.12) Gecko/20101027 Ubuntu/10.04 (lucid) Firefox/3.6.12", "Accept":"text/html"}
 
 	def __init__(self,ip=None):
-		if( ip != None ):
+		if (ip != None):
 			self.populate(ip)
 
 	def populate(self,ip):
@@ -24,23 +23,25 @@ class sphPBL:
 		return 0
 
 	def _fetch_pbl(self):
+		if (self.ip == None):
+			return 1
 		req_url = "http://www.spamhaus.org/query/bl?ip=%s" % self.ip
 		req = urllib2.Request(req_url,None,self._header)
 		page = urllib2.urlopen(req).read()
 		reg = re.compile('<LI><a href="([^"]+)">PBL([0-9]+)</a><br>')
 		m = re.findall(reg, page)
-		if ( m != [] ):
+		if (m != []):
 			self.pbl_url = m[0][0]
 			self.pbl_num = m[0][1]
 		return 0
 
 	def _fetch_cidr(self):
-		if( self.pbl_url == None ):
+		if (self.pbl_url == None):
 			return 1
 		req = urllib2.Request(self.pbl_url,None,self._header)
 		page = urllib2.urlopen(req).read()
 		reg = re.compile('<font color="red">([^<]+)</font> is listed on the Policy Block List \(PBL\)</span><br>')
 		m = re.findall(reg, page)
-		if ( m != [] ):
+		if (m != []):
 			self.cidr = m[0]
 		return 0
