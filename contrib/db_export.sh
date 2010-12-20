73b090405fb8eaafa78fb0a2ba5c266883e93a78
Andrea Gelmini <andrea.gelmini@gelma.net>
2010-12-20 16:38:46 +0100

    Updated db schema/dump.

--- contrib/db_export.sh
+++ contrib/db_export.sh
@@ -2,3 +2,4 @@
 
 [ -e db_fucklog_MySQL_schema.sql ] && /usr/bin/mysqldump --skip-extended-insert  --add-drop-database --add-drop-table   --lock-all-tables  --quote-names -u fucklog -p fucklog > db_fucklog_MySQL_schema.sql
 [ -e pbl_list.txt ] && /usr/bin/mysql -u fucklog -p -e 'select CIDR from PBL' fucklog |sort -n -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4 > pbl_list.txt
+mirrordir -v /home/gelma/siae/backup/linode/var/spool/fucklog/dnswl/ dnswl/
